df7c5c5ed51f173ad58c7545cca01df8a3325344
Richard Fuchs <fuchsrichard@gmail.com>
2015-11-01 12:00:45 +0100

    added name and coordiante to location

--- src/test/java/org/wahlzeit/model/Location.java
+++ src/test/java/org/wahlzeit/model/Location.java
@@ -1,5 +1,45 @@
 package org.wahlzeit.model;
 
 public class Location {
+	private String name;
+	private Coordinate coordinate;
+
+
+	/**
+	 * @methodtype constructor
+	 */
+	public Location(double latitude, double longitude, String name) {
+		this.coordinate = new Coordinate(latitude, longitude);
+		this.name = name;
+	}
+	
+	/**
+	 * @methodtype get
+	 */
+	public Coordinate getCoordinate() {
+		return coordinate;
+	}
+
+	/**
+	 * @methodtype set
+	 */
+	public void setCoordinate(Coordinate coordinate) {
+		this.coordinate = coordinate;
+	}
+
+	/**
+	 * @methodtype get
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @methodtype set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
 
 }
