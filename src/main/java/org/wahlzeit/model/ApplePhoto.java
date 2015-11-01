910be93f0d52a5de46a3585be13e3aaacb7a4425
Richard Fuchs <fuchsrichard@gmail.com>
2015-11-01 12:24:05 +0100

    members, getter, setter and constructor added to ApplePhotos

--- src/main/java/org/wahlzeit/model/ApplePhoto.java
+++ src/main/java/org/wahlzeit/model/ApplePhoto.java
@@ -1,5 +1,76 @@
 package org.wahlzeit.model;
 
-public class ApplePhoto {
+public class ApplePhoto extends Photo {
 
+	
+	private double size;
+	private double weight;
+	private String Name;
+
+
+	/**
+	* @methodtype constructor
+	*/
+	public ApplePhoto(){
+		super();
+	}
+	
+
+	/**
+	* @methodtype constructor
+	*/
+	public ApplePhoto(PhotoId id) {
+	super(id);
+	}
+	
+	/**
+	* @methodtype constructor
+	*/
+	public ApplePhoto(PhotoId id, double latitude, double longitude) {
+		super(id, latitude, longitude);
+			}
+	
+	
+	/**
+	* @methodtype get
+	*/
+	public double getSize() {
+		return size;
+	}
+
+	/**
+	* @methodtype set
+	*/
+	public void setSize(double size) {
+		this.size = size;
+	}
+
+	/**
+	* @methodtype get
+	*/
+	public double getWeight() {
+		return weight;
+	}
+
+	/**
+	* @methodtype set
+	*/
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+	
+	/**
+	* @methodtype get
+	*/
+	public String getName() {
+		return Name;
+	}
+
+	/**
+	* @methodtype set
+	*/
+	public void setName(String name) {
+		Name = name;
+	}
 }
