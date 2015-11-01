ab1ce311b6ae68ea43f70cb0b8c5df22dba8a83a
Richard Fuchs <fuchsrichard@gmail.com>
2015-11-01 21:14:50 +0100

    started AppleCultivars

---
+++ src/main/java/org/wahlzeit/model/AppleCultivars.java
@@ -0,0 +1,35 @@
+package org.wahlzeit.model;
+
+import org.wahlzeit.utils.EnumValue;
+
+public enum AppleCultivars implements EnumValue {
+	GLOSTER, BRAEBURN, GALA, IDARED, JONAGOLD,
+	GOLDENDELICIOUS, BOSKOOP, ELSTAR, PINKLADY, GRANNYSMITH,
+	;
+
+	
+	@Override
+	public int asInt() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String asString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EnumValue[] getAllValues() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTypeName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
