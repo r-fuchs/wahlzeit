cee7206726d15ef9f5536c7d269623c7a2fb69a3
Richard Fuchs <fuchsrichard@gmail.com>
2015-11-08 18:03:26 +0100

    added members and getter+setter

--- src/main/java/org/wahlzeit/model/CartesianCoordinate.java
+++ src/main/java/org/wahlzeit/model/CartesianCoordinate.java
@@ -2,6 +2,34 @@ package org.wahlzeit.model;
 
 public class CartesianCoordinate implements Coordinate {
 
+	private double x;
+	private double y;
+	private double z;
+	
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public double getZ() {
+		return z;
+	}
+
+	public void setZ(double z) {
+		this.z = z;
+	}
+
 	@Override
 	public double getDistance(Coordinate c) {
 		// TODO Auto-generated method stub
--- src/main/java/org/wahlzeit/model/SphericCoordinate.java
+++ src/main/java/org/wahlzeit/model/SphericCoordinate.java
@@ -2,6 +2,34 @@ package org.wahlzeit.model;
 
 public class SphericCoordinate implements Coordinate {
 
+	private double latitude;
+	private double longitude;
+	private double radius;
+	
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
 	@Override
 	public double getDistance(Coordinate c) {
 		return 0;
