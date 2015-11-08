d19be9a69064b04156b4e2449df9f08b960c8ec8
Richard Fuchs <fuchsrichard@gmail.com>
2015-11-08 18:52:06 +0100

    added EARTHRADIUS = 6371;

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -2,6 +2,8 @@ package org.wahlzeit.model;
 
 public interface Coordinate {
 
+	public static double EARTHRADIUS = 6371;
+
 	public double getDistance(Coordinate c);
 	public boolean isEqual(Coordinate c);
 	
