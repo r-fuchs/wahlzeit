5ff8270d4a5d38fca76facd893c1484adc2441e5
Richard Fuchs <fuchsrichard@gmail.com>
2015-11-10 21:57:11 +0100

    added AbstractCoordinate

---
+++ src/main/java/org/wahlzeit/model/AbstractCoordinate.java
@@ -0,0 +1,35 @@
+package org.wahlzeit.model;
+
+public abstract class AbstractCoordinate implements Coordinate {
+
+	@Override
+	public double getDistance(Coordinate c) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEqual(Coordinate c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getZ() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
